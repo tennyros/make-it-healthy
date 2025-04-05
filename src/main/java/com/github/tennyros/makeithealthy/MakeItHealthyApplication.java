11b912d5c0777a374b8e074097ed8987193db983
wendelca <vvkamalov_obs@mail.ru>
2025-04-05 16:28:30 +0500

    feat(entities): add necessary dependencies, entities, properties in application.yml, liquibase create table migrations
    - add necessary dependencies to pom.xml;
    - add application.yml file with properties settings;
    - add docker-compose.yml file for PostgreSQL db start;
    - add User, Meal, FoodInTake entities and necessary Enums;
    - changelog liquibase migration for tables.

---
+++ src/main/java/com/github/tennyros/makeithealthy/MakeItHealthyApplication.java
@@ -0,0 +1,13 @@
+package com.github.tennyros.makeithealthy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeItHealthyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakeItHealthyApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/github/tennyros/makeithealthy/entity/MealType.java
@@ -0,0 +1,7 @@
+package com.github.tennyros.makeithealthy.entity;
+
+public enum MealType {
+
+    BREAKFAST, LUNCH, DINNER, SNACK
+
+}
