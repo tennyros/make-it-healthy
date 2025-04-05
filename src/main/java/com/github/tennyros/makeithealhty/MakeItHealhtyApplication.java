beb3594de0feaeed3e65eb694fe9cda34636f020
wendelca <vvkamalov_obs@mail.ru>
2025-04-05 10:38:47 +0500

    init commit

---
+++ src/main/java/com/github/tennyros/makeithealhty/MakeItHealhtyApplication.java
@@ -0,0 +1,13 @@
+package com.github.tennyros.makeithealhty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeItHealhtyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeItHealhtyApplication.class, args);
+	}
+
+}
